1f7e4819468b5a3b3946895704c60b8355d0250c
osdifera <neezcafe@gmail.com>
2021-12-24 13:36:47 -0600

    chore: updating file name to pickup list

--- services/list_service.go
+++ services/list_service.go
@@ -3,5 +3,5 @@ package services
 import "github.com/gin-gonic/gin"
 
 func GetListFile(c *gin.Context) {
-	c.FileAttachment("./file.txt", "file.txt")
+	c.FileAttachment("./romano.m3u", "romano.m3u")
 }
